3f2a370a36b94cf4cf11f28329e14fb282c17a28
Erik Hanson <erik@erikhanson.dev>
2024-07-16 15:05:02 -0700

    Add title meta tag

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,12 +4,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <meta name="title" content="{{ $title ?? 'Erik Hanson' }}">
     <meta name="description" content="The personal website of Erik Hanson. Thanks for visiting! 👋">
 
     <!-- Open Graph Meta Tags -->
     <meta property="og:url" content="https://erikhanson.dev/">
     <meta property="og:type" content="website">
-    <meta name="og:title" content="Erik Hanson">
+    <meta name="og:title" content="{{ $title ?? 'Erik Hanson' }}">
     <meta name="og:description" content="The personal website of Erik Hanson. Thanks for visiting! 👋">
     <meta property="og:image" content="https://erikhanson.dev/images/og_card.png">
 
@@ -17,7 +18,7 @@
     <meta name="twitter:card" content="summary_large_image">
     <meta property="twitter:domain" content="erikhanson.dev">
     <meta property="twitter:url" content="https://erikhanson.dev/">
-    <meta name="twitter:title" content="Erik Hanson">
+    <meta name="twitter:title" content="{{ $title ?? 'Erik Hanson' }}">
     <meta name="twitter:description" content="The personal website of Erik Hanson. Thanks for visiting! 👋">
     <meta name="twitter:image" content="https://erikhanson.dev/images/og_card.png">
 
